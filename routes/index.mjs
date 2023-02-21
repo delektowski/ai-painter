24b604336994cfd100eba98f1f24345851ed9722
Marcin Delektowski <marcin.delektowski@gmail.com>
2023-02-21 21:42:20 +0100

    fix prompt error

--- routes/index.mjs
+++ routes/index.mjs
@@ -1,22 +1,24 @@
 import express from "express";
-import { saveImgDataToDb } from "../db/database.mjs";
-import { handleCreatePainting } from "../ai/handleCreatePainting.mjs";
+import {saveImgDataToDb} from "../db/database.mjs";
+import {handleCreatePainting} from "../ai/handleCreatePainting.mjs";
 
 const router = express.Router();
 
+let prompt = '';
+
 router.get("/", (req, res) => {
-  res.render("pages/index", { imgSrc: null });
+    res.render("pages/index", {imgSrc: null, prompt});
 });
 
 router.post("/", async (req, res) => {
-  let prompt = req.body?.prompt || "";
+    prompt = req.body?.prompt;
 
-  if (prompt) {
-    const date = new Date().toISOString();
-    const imgData = await handleCreatePainting(prompt);
-    res.render("pages/index", { imgSrc: imgData.imgSrc, prompt });
-    saveImgDataToDb(prompt, date, imgData);
-  }
+    if (prompt) {
+        const date = new Date().toISOString();
+        const imgData = await handleCreatePainting(prompt);
+        res.render("pages/index", {imgSrc: imgData.imgSrc, prompt});
+        saveImgDataToDb(prompt, date, imgData);
+    }
 });
 
 export default router;
